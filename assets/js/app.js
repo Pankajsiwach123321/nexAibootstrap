2186e3be5f053bf81d22bfb40c231739dbd0b02f
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-02-28 10:22:10 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+let nav = document.querySelector(".nav");
+let menubar = document.querySelector(".mobileshow");
+let finish = document.querySelectorAll(".finish");
+let overlay = document.querySelector(".over-layer")
+let bgbody = document.querySelector(".bg-bodys");
+let fix1 = document.querySelector(".fix1");
+let fix2 = document.querySelector(".fix2");
+let fix3 = document.querySelector(".fix3");
+nav.addEventListener("click", function () {
+    menubar.classList.toggle("left-0")
+    fix1.classList.toggle("fixer1")
+    fix2.classList.toggle("fixer2")
+    fix3.classList.toggle("fixer3")
+})
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("d-none");
+    document.body.classList.remove("overflow_hidden");
+}, 4000);
+// backtotop
+function backtop() {
+    window.scrollTo(0, 0);
+}
+window.addEventListener("scroll", function () {
+    const mybackto = document.getElementById("backtops");
+    if (window.scrollY > 500) {
+        mybackto.style.display = "block";
+    }
+    else {
+        mybackto.style.display = "none";
+    }
+});
